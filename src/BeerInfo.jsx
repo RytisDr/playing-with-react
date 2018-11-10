1b89816ce897c1e01eec940a317ead70b7e27225
RytisDr <r.drazdauskas@gmail.com>
2018-11-10 16:16:44 +0100

    Now lists beers on tap.

--- src/BeerInfo.jsx
+++ src/BeerInfo.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 export default class BeerInfo extends React.Component {
   render() {
-    function mapBeertypes(beertypes) {}
+    function mapBeertypes(beertypes) {
+      let beerNames = beertypes.map(beer => {
+        return <li key={beer.name}>{beer.name}</li>;
+      });
+      return beerNames;
+    }
     return (
       <section>
-        <h1>Beers we serve today:</h1>
+        <h1>Beers we are pouring right now:</h1>
         <ul>{mapBeertypes(this.props.beertypes)}</ul>
       </section>
     );
--- src/bartenders.jsx
+++ src/bartenders.jsx
@@ -2,6 +2,7 @@ import React from "react";
 export default class Bartenders extends React.Component {
   render() {
     function mapBartenders(bartenders) {
+      console.log(bartenders);
       let working = bartenders.map(e => {
         return (
           <li key={e.name}>
